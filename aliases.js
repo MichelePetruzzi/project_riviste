c40088f00f0537f07503183992396216f62de68b
MichelePetruzzi <petruzzi.michele@panetti.pitagora.edu.it>
2021-04-15 20:45:00 +0200

    commit :)

--- aliases.js
+++ aliases.js
@@ -3,8 +3,8 @@ const path = require('path');
 const obj = {
 	alias: {
 		"@node_modules": path.resolve(__dirname, 'node_modules'),
-		"@js": path.resolve(__dirname, 	'src/js'),
-		"@css": path.resolve(__dirname, 	'src/css'),
+		"@js": path.resolve(__dirname, 'src/js'),
+		"@css": path.resolve(__dirname, 'src/css'),
 	}
 }
 
